2184fc021213b71bb35552755e7ff4295de6e88a
0xdr30 <adamson.will21@gmail.com>
2023-11-08 12:02:54 -0800

    new

--- scripting/windows_main.ps1
+++ scripting/windows_main.ps1
@@ -2,3 +2,11 @@ Set-ExecutionPolicy remotesigned
 Install-Module PSWindowsUpdate -force
 Get-WindowsUpdate
 Install-WindowsUpdate -force
+New-Item -Path "C:\" -Name "script" -ItemType Directory
+$URL="https://whoppenheimer.com/scripting/windows_main.inf"
+$Path="C:\script\windows.inf"
+Invoke-WebRequest -URL $URL -OutFile $Path
+secedit.exe /configure /db %windir%\security\local.sdb /cfg C:\script\windows.inf
+
+$shares = get-WmiObject -class Win32_Share where {($_.name -ne "ADMIN$") -or ($_.name -ne "IPC$") -or ($_.name -ne "C$") -or ($_.name -ne "PRINT$")}
+Write-Host $shares
