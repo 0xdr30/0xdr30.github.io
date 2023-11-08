86e60f0b6942a8fa7884e42b13593c43b0318b95
dr30 <116238443+0xdr30@users.noreply.github.com>
2023-11-08 13:42:11 -0800

    Update windows_main.ps1

--- scripting/windows_main.ps1
+++ scripting/windows_main.ps1
@@ -1,12 +1,41 @@
-Set-ExecutionPolicy remotesigned
-Install-Module PSWindowsUpdate -force
-Get-WindowsUpdate
-Install-WindowsUpdate -force
-New-Item -Path "C:\" -Name "script" -ItemType Directory
-$URL="https://whoppenheimer.com/scripting/windows_main.inf"
-$Path="C:\script\windows.inf"
-Invoke-WebRequest -URL $URL -OutFile $Path
-secedit.exe /configure /db %windir%\security\local.sdb /cfg C:\script\windows.inf
+function windows_update(){
+    Set-ExecutionPolicy remotesigned
+    Install-Module PSWindowsUpdate -force
+    Get-WindowsUpdate
+    Install-WindowsUpdate -force
+}
 
-$shares = get-WmiObject -class Win32_Share where {($_.name -ne "ADMIN$") -or ($_.name -ne "IPC$") -or ($_.name -ne "C$") -or ($_.name -ne "PRINT$")}
-Write-Host $shares
+function remove_shares(){
+
+    Remove-Item "C:\script" -Recurse
+    New-Item -Path "C:\" -Name "script" -ItemType Directory
+    New-Item "C:\script\log.txt"
+    New-Item "C:\script\shares.txt"
+    $Path = "C:\script\shares.txt"
+    $Log = "C:\script\log.txt"
+    get-WmiObject -class Win32_Share | Where-Object -Property Name -ne 'C$' | Where-Object -Property Name -ne 'ADMIN$' | Where-Object -Property Name -ne 'IPC$' | Select-Object -Property Name > $Path
+    (get-content $Path) -replace "Name","" | Out-File $Path
+    (get-content $Path) -replace "----","" | Out-File $Path
+    $shares = $Path
+    foreach ($share in Get-Content $Path){
+        if(-not [string]::IsNullOrWhiteSpace($share)){
+            Remove-SmbShare -Name $share -force
+            Write-Host ""
+            Write-Host "Removed Share '$share'!" | Out-File $Log
+        }
+    }
+}
+
+function update_secpol(){
+    $URL="https://whoppenheimer.com/scripting/windows_main.inf"
+    $Path="C:\script\windows.inf"
+    Invoke-WebRequest -URI $URL -OutFile $Path
+    secedit.exe /configure /db %windir%\security\local.sdb /cfg $Path
+}
+
+function main(){
+    remove_shares
+    update_secpol
+}
+
+main
