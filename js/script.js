cd19686e79c50c6e84e4cb8b4586341eb286c98a
dr30 <adamson.will21@gmail.com>
2023-12-27 05:06:38 -0800

    updated navbar with responsive design from w3

--- js/script.js
+++ js/script.js
@@ -8,7 +8,17 @@ function startTime() {
   document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
   setTimeout(startTime, 1000);
 }
+
 function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
+}
+
+function responsive() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
